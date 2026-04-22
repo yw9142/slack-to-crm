346e8d9bdffdba4f1b9b72205b9bf437d218d001
Yonghun Park <yw9142@gmail.com>
2026-04-22 10:40:07 +0900

    Initial commit from Create Twenty App

---
+++ src/constants/universal-identifiers.ts
@@ -0,0 +1,4 @@
+export const APP_DISPLAY_NAME = 'Slack to CRM';
+export const APP_DESCRIPTION = 'Slack CRM agent powered by Twenty MCP';
+export const APPLICATION_UNIVERSAL_IDENTIFIER = '901a7549-e439-43c2-b9b4-772cc04306d9';
+export const DEFAULT_ROLE_UNIVERSAL_IDENTIFIER = '78d60d64-b81e-4aa1-9258-e3fc7d962c7a';
